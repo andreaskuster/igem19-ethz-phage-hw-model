5cb0791218c792e090d71fb81fc43234531cb16d
andreas <mail@andreaskuster.ch>
2019-09-04 12:30:50 +0200

    Add default functions and lb consumption to host parameters.

--- yeast_assembly/host.py
+++ yeast_assembly/host.py
@@ -5,15 +5,20 @@ class Host():
 
     def __init__(self,
                  growth_rate: float,
-                 c0: int,
-                 t_dep,
-                 lb_dep):
+                 c0: int = 1,
+                 t_dep = lambda: 1.0,
+                 lb_dep = lambda: 1.0,
+                 lb_cons = lambda: 0.0):
         """
 
         :param growth_rate:
-        :param c0: initial host concentration
+        :param c0:
+        :param t_dep:
+        :param lb_dep:
+        :param lb_cons:
         """
         self.growth_rate = growth_rate
         self.c0 = c0
         self.t_dep = t_dep
         self.lb_dep = lb_dep
+        self.lb_cons = lb_cons
