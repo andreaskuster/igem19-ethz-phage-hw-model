eb23dfad9ec30869b05f0a79a09d31c601e380da
andreas <mail@andreaskuster.ch>
2019-09-12 12:05:10 +0200

    Change to zero-naming. Comment code.

--- hardware/esc/esc_i2c.c
+++ hardware/esc/esc_i2c.c
@@ -1,68 +1,84 @@
 #include <Servo.h>
 #include<Wire.h>
 
+/*
+    Arduino ESC I2C Controller
+
+    This device listens at address 77 on the i2c bus and sends PPM signals to the ESC controller for temperature control.
+*/
+
 // instantiate the servo object modules
+Servo ESC0;
 Servo ESC1;
 Servo ESC2;
-Servo ESC3;
 
 // i2c data receive event handler
-void receiveEvent (int bytes)
-{
-    if(bytes == 2){
+void receiveEvent (int bytes) {
 
+    // check if received data has correct length
+    if(bytes == 2) {
+
+        // read values of the tuple (device id, control value)
         byte device = Wire.read();
-        int value = map(Wire.read(),150,200,0,200);
+
+        // map value from [0, 200] to [1500, 2000]
+        int value = map(Wire.read(), 150, 200, 0, 200);
         value *= 10;
+
+        // choose the device according to the id
         switch(device){
+            case 0:
+                // send PPM value to ESC1
+                ESC0.writeMicroseconds(value);
+                break;
             case 1:
+                // send PPM value to ESC2
                 ESC1.writeMicroseconds(value);
                 break;
             case 2:
+                // send PPM value to ESC3
                 ESC2.writeMicroseconds(value);
-                break;
-            case 3:
-                ESC3.writeMicroseconds(value);
                 break;
         }
 
-          // print received control command
-          Serial.print("device:");
-          Serial.print(device);
-          Serial.print(" value:");
-          Serial.print(value);
-          Serial.println();
+         // print received control command
+         Serial.print("device:");
+         Serial.print(device);
+         Serial.print(" value:");
+         Serial.print(value);
+         Serial.println();
     } else {
-        //
+
+        // false length: read all and void
         for(int i = 0; i < bytes; i++){
          Wire.read();
         }
     }
 }
 
-
 void setup() {
 
-  // start serial debug output
-  Serial.begin(9600);
-  Serial.println("start device");
+    // start serial debug output
+    Serial.begin(9600);
+    Serial.println("start device");
 
-  // init esc controller
-  Serial.println("init esc");
-  ESC1.attach(9);
-  ESC2.attach(10);
-  ESC3.attach(11);
-  ESC1.writeMicroseconds(1750);
-  ESC2.writeMicroseconds(1750);
-  ESC3.writeMicroseconds(1750);
+    // init esc controller
+    Serial.println("init esc");
+    ESC0.attach(9);
+    ESC1.attach(10);
+    ESC2.attach(11);
+    ESC0.writeMicroseconds(1750);
+    ESC1.writeMicroseconds(1750);
+    ESC2.writeMicroseconds(1750);
 
-  // start i2c slave at address 77
-  Serial.println("init i2c");
-  Wire.begin(77);
-  Wire.onReceive(receiveEvent);
+    // start i2c slave at address 77
+    Serial.println("init i2c");
+    Wire.begin(77);
+    Wire.onReceive(receiveEvent);
 
-  Serial.println("wait for esc");
-  delay(2000);
+    // give the escs some time to set up
+    Serial.println("wait for esc");
+    delay(2000);
 }
 
 void loop() {
