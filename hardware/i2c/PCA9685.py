bc3bc8dff8bee43d154c5a147673889b7cf080ac
andreas <mail@andreaskuster.ch>
2019-09-16 14:55:15 +0200

    Add initial prototype implementation of the 16 channel PWM module.

--- hardware/i2c/PCA9685.py
+++ hardware/i2c/PCA9685.py
@@ -1,13 +1,18 @@
-import time
-import Adafruit_PCA9685
+from board import SCL, SDA
+import busio
 
-pwm = Adafruit_PCA9685.PCA9685()
+# Import the PCA9685 module.
+from adafruit_pca9685 import PCA9685
 
-while True:
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(8, 0, 0)
-    time.sleep(1)
-    pwm.set_pwm(8, 0, 4096)
-    time.sleep(1)
+# Create the I2C bus interface.
+i2c_bus = busio.I2C(SCL, SDA)
 
+# Create a simple PCA9685 class instance.
+pca = PCA9685(i2c_bus)
 
+# Set the PWM frequency to 60hz.
+pca.frequency = 60
+
+# Set the PWM duty cycle for channel zero to 50%. duty_cycle is 16 bits to match other PWM objects
+# but the PCA9685 will only actually give 12 bits of resolution.
+pca.channels[8].duty_cycle = 0xffff
