3545c89322b53e07210929e3676f0f13df255ab4
andreas <mail@andreaskuster.ch>
2019-09-16 14:28:46 +0200

    Extract actual temperature value.

--- hardware/one_wire/DS18B20.py
+++ hardware/one_wire/DS18B20.py
@@ -1,21 +1,46 @@
 import os
 import time
+import re
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-sensor1 = '/sys/bus/w1/devices/28-0114536b03aa/w1_slave'
-sensor2 = '/sys/bus/w1/devices/28-80000026f3d8/w1_slave'
-sensor3 = '/sys/bus/w1/devices/28-0114534081aa/w1_slave'
+sensors = {
+    [
+        {
+            'name': 'sensor1',
+            'socket': '/sys/bus/w1/devices/28-0114536b03aa/w1_slave'
+        },
+        {
+            'name': 'sensor2',
+            'socket': '/sys/bus/w1/devices/28-80000026f3d8/w1_slave'
+        },
+        {
+            'name': 'sensor3',
+            'socket': '/sys/bus/w1/devices/28-0114534081aa/w1_slave'
+        }
+    ]
+}
 
 
-def temp_raw(socket_path: str):
-    with open(socket_path) as socket:
+def raw_sensor_data(socket_path: str) -> str:
+    with open(socket_path, 'r') as socket:
         lines = socket.readlines()
     return lines
 
 
+def extract_temperature(raw_sensor_data: str) -> float:
+    position = raw_sensor_data[1].find('t=')
+    if position != -1:
+        temperature = raw_sensor_data[1].strip()[position + 2:]
+    return float(temperature)/1000.0
+
+
 while True:
-    for sensor in [sensor1, sensor2, sensor3]:
-        print(temp_raw(sensor))
+    for sensor in sensors:
+        print("{}: {}°C".format(sensor['name'], extract_temperature(raw_sensor_data(sensor['socket']))))
     time.sleep(10)
+
+
+
+# credits: https://thepihut.com/blogs/raspberry-pi-tutorials/ds18b20-one-wire-digital-temperature-sensor-and-the-raspberry-pi
