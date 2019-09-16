6d736b7fe0fac891ed85fb85fe02f31756ddc724
andreas <mail@andreaskuster.ch>
2019-09-16 14:03:32 +0200

    Fix missing argument.

--- hardware/one_wire/DS18B20.py
+++ hardware/one_wire/DS18B20.py
@@ -9,7 +9,7 @@ sensor2 = '/sys/bus/w1/devices/28-80000026f3d8/w1_slave'
 sensor3 = '/sys/bus/w1/devices/28-0114534081aa/w1_slave'
 
 
-def temp_raw(socket_path):
+def temp_raw(socket_path: str):
     with open(socket_path) as socket:
         lines = socket.readlines()
     return lines
@@ -17,5 +17,5 @@ def temp_raw(socket_path):
 
 while True:
     for sensor in [sensor1, sensor2, sensor3]:
-        print(temp_raw())
+        print(temp_raw(sensor))
     time.sleep(10)
