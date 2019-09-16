e3d3ed451055bfc10cd48b85b935136c404a6ae7
andreas <mail@andreaskuster.ch>
2019-09-16 14:02:00 +0200

    Add initial prototype code for fetching temperature sensor data from the 1-wire bus.

---
+++ hardware/one_wire/DS18B20.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+sensor1 = '/sys/bus/w1/devices/28-0114536b03aa/w1_slave'
+sensor2 = '/sys/bus/w1/devices/28-80000026f3d8/w1_slave'
+sensor3 = '/sys/bus/w1/devices/28-0114534081aa/w1_slave'
+
+
+def temp_raw(socket_path):
+    with open(socket_path) as socket:
+        lines = socket.readlines()
+    return lines
+
+
+while True:
+    for sensor in [sensor1, sensor2, sensor3]:
+        print(temp_raw())
+    time.sleep(10)
