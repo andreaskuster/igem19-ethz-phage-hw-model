0d1732bba81611f15f53b2337aa2dc5f0747c58e
Andreas Kuster <mail@andreaskuster.ch>
2019-09-17 15:58:42 +0200

    Add functionality to set/reset individual I/O ports.

--- hardware/i2c/PCF8574.py
+++ hardware/i2c/PCF8574.py
@@ -14,12 +14,22 @@ def set_all():
     bus.write_byte(_DEVICE_ADDRESS, 0xff)
 
 
+def set(port: int):
+    # read register
+    state = bus.read_byte(_DEVICE_ADDRESS)
+    # set port
+    bus.write_byte(_DEVICE_ADDRESS, state | (0x1 << port))
+
+
+def clear(port: int):
+    # read register
+    state = bus.read_byte(_DEVICE_ADDRESS)
+    # clear port
+    bus.write_byte(_DEVICE_ADDRESS, state & (0xff ^ (0x1 << port)))
+
 
 # note: PCF8574 uses 'sinking': 0 -> on, 1 -> off
 
+set_all()
+clear(1)
 
-bus.write_byte(_DEVICE_ADDRESS, 0xff)
-
-print(bus.read_byte(_DEVICE_ADDRESS))
-
-#bus.write_byte(0x38, 0x01 << port)
