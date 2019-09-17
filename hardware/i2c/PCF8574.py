bd91909c22e8cfdebde262fce7c3e66fbf07c84a
andreas <mail@andreaskuster.ch>
2019-09-17 15:41:29 +0200

    Implement I/O clear/set functionality.

--- hardware/i2c/PCF8574.py
+++ hardware/i2c/PCF8574.py
@@ -1,16 +1,25 @@
 import smbus
 
-bus = smbus.SMBus(1)
+_BUS_NO = 1
+_DEVICE_ADDRESS = 0x38
 
-def clear():
-    bus.write_byte(0x38, 0x00)
+bus = smbus.SMBus(_BUS_NO)
 
 
-#port = 4  # 0..7
+def clear_all():
+    bus.write_byte(_DEVICE_ADDRESS, 0x00)
+
+
+def set_all():
+    bus.write_byte(_DEVICE_ADDRESS, 0xff)
+
 
 
 # note: PCF8574 uses 'sinking': 0 -> on, 1 -> off
 
 
-bus.write_byte(0x38, 0xff)
+bus.write_byte(_DEVICE_ADDRESS, 0xff)
+
+print(bus.read_byte(_DEVICE_ADDRESS))
+
 #bus.write_byte(0x38, 0x01 << port)
