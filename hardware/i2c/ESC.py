5a16ec19cd11d628973c31e0a0b8f512b2905023
Andreas Kuster <mail@andreaskuster.ch>
2019-09-17 17:02:53 +0200

    Add methods to control the ESCs.

--- hardware/i2c/ESC.py
+++ hardware/i2c/ESC.py
@@ -1,11 +1,25 @@
 import smbus
 
-bus = smbus.SMBus(1)
+_BUS_NO = 1
+_DEVICE_ADDRESS = 0x07
 
-#bus.write_byte_data(0x07, 0x0, 0x00)
-#bus.write_byte_data(0x07, 0x0, 0x96)
+bus = smbus.SMBus(_BUS_NO)
 
-val = 100
-bus.write_i2c_block_data(0x07, 0x00, [val])
-bus.write_i2c_block_data(0x07, 0x01, [val])
-bus.write_i2c_block_data(0x07, 0x02, [val])
+
+def set_value(device: int, value: int):
+    # device: {1,2,3}
+    # value: input: [-100, 100] (needs to be mapped to [0, 200]
+    bus.write_i2c_block_data(_DEVICE_ADDRESS, device, [value + 100])
+
+
+def stop_esc(device: int):
+    set_value(device, 0)
+
+
+def full_forward(device: int):
+    set_value(device, 100)
+
+
+def full_reverse(device: int):
+    set_value(device, -100)
+
