4f049341cfd00e7f4459f4c8a815a817719b72a5
andreas <mail@andreaskuster.ch>
2019-09-17 17:53:04 +0200

    Rewrite PCF8574 call functions to meet global api standard.

--- hardware/i2c/PCF8574.py
+++ hardware/i2c/PCF8574.py
@@ -1,39 +1,78 @@
 import time
 import smbus
 
-_BUS_NO = 1
 _DEVICE_ADDRESS = 0x38
 
-bus = smbus.SMBus(_BUS_NO)
 
-
-def clear_all():
+def clear_all(bus: smbus):
+    """
+    Resets all eight I/Os to zero.
+    :param bus: smbus instance
+    """
     bus.write_byte(_DEVICE_ADDRESS, 0x00)
 
 
-def set_all():
+def set_all(bus: smbus):
+    """
+    Sets all eight I/Os to one.
+    :param bus:
+    """
+
     bus.write_byte(_DEVICE_ADDRESS, 0xff)
 
 
-def set(port: int):
+def set(bus: smbus,
+        port: int):
+    """
+    Sets an individual I/O to one.
+    :param bus: smbus instance
+    :param port: I/O port (0,..,7)
+    """
     # read register
     state = bus.read_byte(_DEVICE_ADDRESS)
     # set port
     bus.write_byte(_DEVICE_ADDRESS, state | (0x1 << port))
 
 
-def clear(port: int):
+def clear(bus: smbus,
+          port: int):
+    """
+    Clears an individual I/O to zero.
+    :param bus: smbus instance
+    :param port: I/O port (0,..,7)
+    """
     # read register
     state = bus.read_byte(_DEVICE_ADDRESS)
     # clear port
     bus.write_byte(_DEVICE_ADDRESS, state & (0xff ^ (0x1 << port)))
 
 
+def toggle(bus: smbus,
+          port: int):
+    """
+    Toggles an individual I/O port.
+    :param bus: smbus instance
+    :param port: I/O port (0,..,7)
+    """
+    # read register
+    state = bus.read_byte(_DEVICE_ADDRESS)
+    # clear port
+    bus.write_byte(_DEVICE_ADDRESS, state ^ (0x1 << port))
+
+
 if __name__ == "__main__":
+
+    # instantiate i2c smbus
+    _BUS_NO = 1
+    bus = smbus.SMBus(_BUS_NO)
+
+    # run all on / all off interval
     while True:
-        set_all()
+        print("set all outputs")
+        set_all(bus)
         time.sleep(10)
-        clear_all()
+        print("reset all outputs")
+        clear_all(bus)
         time.sleep(10)
 
 # note: PCF8574 uses 'sinking': 0 -> on, 1 -> off
