f219e572131c060394c9473a8bbcb828c3efd9f6
andreas <mail@andreaskuster.ch>
2019-09-17 22:27:08 +0200

    Adjust comments.

--- hardware/i2c/PCA9685.py
+++ hardware/i2c/PCA9685.py
@@ -1,7 +1,5 @@
 from board import SCL, SDA
 import busio
-
-# Import the PCA9685 module.
 from adafruit_pca9685 import PCA9685
 
 # Create the I2C bus interface.
@@ -12,9 +10,6 @@ pca = PCA9685(i2c_bus)
 
 # Set the PWM frequency to 60hz.
 pca.frequency = 60
-
-# Set the PWM duty cycle for channel zero to 50%. duty_cycle is 16 bits to match other PWM objects
-# but the PCA9685 will only actually give 12 bits of resolution.
 
 # peristaltic pumps
 value = 0xffff
