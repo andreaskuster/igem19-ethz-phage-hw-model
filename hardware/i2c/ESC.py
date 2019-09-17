8bab2cb087de2d1709013f7ebdb4331fb3a13a31
andreas <mail@andreaskuster.ch>
2019-09-17 17:40:40 +0200

    Rewrite esc call function to meet global api standard.

--- hardware/i2c/ESC.py
+++ hardware/i2c/ESC.py
@@ -1,25 +1,53 @@
+import time
 import smbus
 
-_BUS_NO = 1
 _DEVICE_ADDRESS = 0x07
 
-bus = smbus.SMBus(_BUS_NO)
+def set_value(bus: SMBus,
+              device: int,
+              value: int):
+    """
+    Sets the speed of the ESC.
+    :param bus: smbus instance
+    :param device: ESC device id {0, 1, 2}
+    :param value: speed value [-100, 100]
+    """
+    bus.write_i2c_block_data(_DEVICE_ADDRESS, device, [value + 100])  # needs to be mapped to [0, 200]
 
 
-def set_value(device: int, value: int):
-    # device: {1,2,3}
-    # value: input: [-100, 100] (needs to be mapped to [0, 200]
-    bus.write_i2c_block_data(_DEVICE_ADDRESS, device, [value + 100])
+def stop_esc(bus: SMBus,
+             device: int):
+    set_value(bus, device, 0)
 
 
-def stop_esc(device: int):
-    set_value(device, 0)
+def full_forward(bus: SMBus,
+                 device: int):
+    set_value(bus, device, 100)
 
 
-def full_forward(device: int):
-    set_value(device, 100)
+def full_reverse(bus: SMBus,
+                 device: int):
+    set_value(bus, device, -100)
 
 
-def full_reverse(device: int):
-    set_value(device, -100)
+if __name__ == "__main__":
 
+    # instantiate i2c smbus
+    _BUS_NO = 1
+    bus = smbus.SMBus(_BUS_NO)
+
+    while True:
+        stop_esc(bus, 0)
+        stop_esc(bus, 1)
+        stop_esc(bus, 2)
+        time.sleep(10)
+        full_forward(bus, 0)
+        full_forward(bus, 1)
+        full_forward(bus, 2)
+        stop_esc(bus, 0)
+        stop_esc(bus, 1)
+        stop_esc(bus, 2)
+        time.sleep(10)
+        full_reverse(bus, 0)
+        full_reverse(bus, 1)
+        full_reverse(bus, 2)
