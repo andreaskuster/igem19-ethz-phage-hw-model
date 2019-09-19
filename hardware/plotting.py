cdf91aee92f237f98738f30ab9ef6859700d5a25
andreas <mail@andreaskuster.ch>
2019-09-19 16:46:39 +0200

    Add initial implementation of real-time graph.

--- hardware/plotting.py
+++ hardware/plotting.py
@@ -1,20 +1,21 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
 
-fig = plt.figure(figsize=(15,10))
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
 
-data = np.genfromtxt('pre_processed_sensor_data.csv', delimiter=',')
-data = [1/x for x in data]
-#np.savetxt("test.csv", [x[0] for x in data])
-
-x = np.linspace(0, len(data)-1, len(data))
-
-plt.ylabel('Sensor Value [Units]')
-plt.xlabel('Time [13s]')
-plt.plot(x, data)
-plt.title('Raw OD Sensor Data')
-plt.legend()
-plt.savefig("raw_sensor.png")
+def animate(i):
+    pullData = open("sampleText.txt","r").read()
+    dataArray = pullData.split('\n')
+    xar = []
+    yar = []
+    for eachLine in dataArray:
+        if len(eachLine)>1:
+            x,y = eachLine.split(',')
+            xar.append(int(x))
+            yar.append(int(y))
+    ax1.clear()
+    ax1.plot(xar,yar)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
-
-
