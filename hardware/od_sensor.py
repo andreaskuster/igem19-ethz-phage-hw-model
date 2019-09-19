51c6ef4e3e8c3e8107d8926fa2044830717734be
andreas <mail@andreaskuster.ch>
2019-09-19 14:44:10 +0200

    Add initial implementation of od sensor measurement loop.

---
+++ hardware/od_sensor.py
@@ -0,0 +1,58 @@
+import time
+import smbus
+from board import SCL, SDA
+import busio
+import numpy as np
+from simple_pid import PID
+
+from hardware.i2c.PCF8574 import clear_all, set_all, set, clear
+from hardware.i2c.PCA9685 import set_pwm
+
+if __name__ == "__main__":
+
+    _BUS_NO = 1
+    smbus = smbus.SMBus(_BUS_NO)
+    # Create the I2C bus interface.
+
+    busio_bus = busio.I2C(SCL, SDA)
+
+    # define target value and reactor id
+    _TARGET_REACTOR = 0
+
+    # initialize log arrays
+    od_log = list()
+
+
+    try:
+        # run control loop till keyboard interrupt (Ctrl + C)
+        starttime = time.time()
+
+        while True:
+
+            # run peristaltic pump for 10s
+            set_pwm(busio_bus, 13, 0xffff)
+
+            # let the biomass settle down for 3s
+            set_pwm(busio_bus, 13, 0x0000)
+
+            # measure dark value
+            set(smbus, 0)
+            time.sleep(0.01)
+            #dark =
+            # measure light value
+            clear(smbus, 0)
+            time.sleep(0.01)
+            #light =
+
+            od = 0.0 #light - dark
+
+            # append log
+            od_log.append(od)
+
+            time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+    except KeyboardInterrupt:
+        # save log files
+        np.savetxt(fname="od.csv", delimiter=",", X=od_log)
+        set_pwm(busio_bus, 13, 0x0000)
+
