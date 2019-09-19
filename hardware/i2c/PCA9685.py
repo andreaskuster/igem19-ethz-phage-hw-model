e2488c7846a6c0d61991aa9bf326e1464b2aa9cb
Andreas Kuster <mail@andreaskuster.ch>
2019-09-19 16:38:36 +0200

    Implement default api to access device.

--- hardware/i2c/PCA9685.py
+++ hardware/i2c/PCA9685.py
@@ -2,16 +2,6 @@ from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
 
-# Create the I2C bus interface.
-i2c_bus = busio.I2C(SCL, SDA)
-
-# Create a simple PCA9685 class instance.
-pca = PCA9685(i2c_bus)
-
-# Set the PWM frequency to 60hz.
-pca.frequency = 60
-
-
 def set_pwm(bus,
             channel,
             value):
@@ -26,27 +16,41 @@ def set_pwm(bus,
     pca.channels[channel].duty_cycle = value
 
 
-# peristaltic pumps
-value = 0xffff
-pca.channels[0].duty_cycle = 0
-pca.channels[1].duty_cycle = 0
-pca.channels[2].duty_cycle = 0
-pca.channels[3].duty_cycle = 0
-pca.channels[4].duty_cycle = 0
-pca.channels[5].duty_cycle = 0
-pca.channels[12].duty_cycle = 0
-pca.channels[13].duty_cycle = 0
-pca.channels[14].duty_cycle = 0
+
+if __name__ == "__main__":
+
+    # Create the I2C bus interface.
+    i2c_bus = busio.I2C(SCL, SDA)
+
+    # Create a simple PCA9685 class instance.
+    pca = PCA9685(i2c_bus)
+
+    # Set the PWM frequency to 60hz.
+    pca.frequency = 60
 
 
-# pc fan
-pca.channels[8].duty_cycle = 0xffff
-pca.channels[9].duty_cycle = 0xffff
-pca.channels[10].duty_cycle = 0xffff
+
+    # peristaltic pumps
+    value = 0xffff
+    pca.channels[0].duty_cycle = 0
+    pca.channels[1].duty_cycle = 0
+    pca.channels[2].duty_cycle = 0
+    pca.channels[3].duty_cycle = 0
+    pca.channels[4].duty_cycle = 0
+    pca.channels[5].duty_cycle = 0
+    pca.channels[12].duty_cycle = 0
+    pca.channels[13].duty_cycle = 0xffff
+    pca.channels[14].duty_cycle = 0
 
 
-# water pump
-pca.channels[6].duty_cycle = 0
-pca.channels[7].duty_cycle = 0
-pca.channels[11].duty_cycle = 0
+    # pc fan
+    pca.channels[8].duty_cycle = 0xffff
+    pca.channels[9].duty_cycle = 0xffff
+    pca.channels[10].duty_cycle = 0x0000
+
+
+    # water pump
+    pca.channels[6].duty_cycle = 0
+    pca.channels[7].duty_cycle = 0
+    pca.channels[11].duty_cycle = 0
 
