29f305cdc8c543b846bb1cebebbaf67df0f18d81
mike <michael.spahr@alumni.epfl.ch>
2019-09-24 12:00:38 +0200

    Add infection rate function to Phage

--- phage.py
+++ phage.py
@@ -17,4 +17,14 @@ class Phage():
         self.adsorption_rate = adsorption_rate
         self.burst_size = burst_size
         self.death_rate = death_rate
+        
+        
+    def infection_rate(self, host_c):
+        
+        """
+        :param host_c: host_concentration
+        :return: infection rate
+        """
+        
+        return self.adsorption_rate * self.c0 * host_c
 
