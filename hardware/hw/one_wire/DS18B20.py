ea853fca025b5b684a0c2c4e720d0a088665ebc6
andreas <mail@andreaskuster.ch>
2019-09-26 00:14:55 +0200

    Modify DS18B20 low level implementation to fit into new project abstraction layer structure.

--- hardware/hw/one_wire/DS18B20.py
+++ hardware/hw/one_wire/DS18B20.py
@@ -1,20 +1,10 @@
 import os
 import time
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
 
-sensors = {
-    'sensor0': {
-        'socket': '/sys/bus/w1/devices/28-0114536b03aa/w1_slave'
-    },
-    'sensor1': {
-        'socket': '/sys/bus/w1/devices/28-80000026f3d8/w1_slave'
-    },
-    'sensor2': {
-        'socket': '/sys/bus/w1/devices/28-0114534081aa/w1_slave'
-    }
-}
+def init():
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
 
 
 def raw_sensor_data(socket_path: str) -> str:
@@ -30,14 +20,16 @@ def extract_temperature(raw_sensor_data: str) -> float:
     return float(temperature) / 1000.0
 
 
-def get_temperature(sensor: int) -> float:
-    return extract_temperature(raw_sensor_data(sensors["sensor" + str(sensor)]['socket']))
+def get_temperature(sensor_mac_addr: str) -> float:
+    return extract_temperature(raw_sensor_data("/sys/bus/w1/devices/" + sensor_mac_addr + "/w1_slave"))
 
 
 if __name__ == "__main__":
     while True:
-        for sensor in sensors:
-            print("{}: {}°C".format(sensor, extract_temperature(raw_sensor_data(sensors[sensor]['socket']))))
+        for sensor in [("Reactor0:", "28-0114536b03aa"),
+                       ("Reactor1:", "28-80000026f3d8"),
+                       ("Reactor2:", "28-0114534081aa")]:
+            print("{}: {}°C".format(sensor[0], extract_temperature(raw_sensor_data(sensor[1]))))
         time.sleep(10)
 
 # credits: https://thepihut.com/blogs/raspberry-pi-tutorials/ds18b20-one-wire-digital-temperature-sensor-and-the-raspberry-pi
