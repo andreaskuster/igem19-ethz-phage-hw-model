838c95632b83fcb4a7a4c40f1a30cd1c94e41e9e
andreas <mail@andreaskuster.ch>
2019-09-26 14:07:51 +0200

    Run measurement infinitely long.

--- hardware/devices/drivers/WaterTemperatureSensor.py
+++ hardware/devices/drivers/WaterTemperatureSensor.py
@@ -39,9 +39,10 @@ class WaterTemperatureSensor(Enum):
 
 
 if __name__ == "__main__":
-    reactors = [WaterTemperatureSensor(WaterTemperatureSensor.REACTOR0),
-                WaterTemperatureSensor(WaterTemperatureSensor.REACTOR1),
-                WaterTemperatureSensor(WaterTemperatureSensor.REACTOR2)]
-    for reactor in reactors:
-        print("Water temperature of {}: {}°C".format(reactor.name, reactor.get_temperature()))
-    time.sleep(10)
+    while True:
+        reactors = [WaterTemperatureSensor(WaterTemperatureSensor.REACTOR0),
+                    WaterTemperatureSensor(WaterTemperatureSensor.REACTOR1),
+                    WaterTemperatureSensor(WaterTemperatureSensor.REACTOR2)]
+        for reactor in reactors:
+            print("Water temperature of {}: {}°C".format(reactor.name, reactor.get_temperature()))
+        time.sleep(10)
