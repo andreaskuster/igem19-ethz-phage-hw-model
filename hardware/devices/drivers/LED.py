b24734244ce14249e4c05977548050782f1985b9
andreas <mail@andreaskuster.ch>
2019-09-26 11:39:57 +0200

    Create initial implementation of the led driver.

---
+++ hardware/devices/drivers/LED.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class LED(Enum):
+
+    REACTOR0 = 0  # TODO: check if the values are correct
+    REACTOR1 = 1
+    REACTOR2 = 2
+
+    def __init__(self):
+        pass
