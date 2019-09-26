bb3ffe51ba047761ec4aacf7a0bb5f41431cb32a
andreas <mail@andreaskuster.ch>
2019-09-26 13:03:30 +0200

    Restructure TCA9548A for use in the driver level.

--- hardware/devices/drivers/hw/i2c/TCA9548A.py
+++ hardware/devices/drivers/hw/i2c/TCA9548A.py
@@ -1,29 +1,25 @@
 import time
 import smbus
 
-_DEVICE_ADDRESS = 0x70
 
+class TCA9548A:
+    _BUS_NO = 1
+    _DEVICE_ADDRESS = 0x70
 
-def switch(bus: smbus,
-        port: int):
-    """
-    Sets an individual I/O to one.
-    :param bus: smbus instance
-    :param port: I2C channel (0,..,7)
-    """
-    # set port
-    bus.write_byte(_DEVICE_ADDRESS, 0x1 << port)
+    bus = smbus.SMBus(_BUS_NO)
 
+    @staticmethod
+    def switch(port: int):
+        """
+        Sets an individual I/O to one.
+        :param port: I2C channel (0,..,7)
+        """
+        # set port
+        TCA9548A.bus.write_byte(TCA9548A._DEVICE_ADDRESS, 0x1 << port)
 
 
 if __name__ == "__main__":
 
-    # instantiate i2c smbus
-    _BUS_NO = 1
-    bus = smbus.SMBus(_BUS_NO)
-
     _CHANNEL = 4
-    
-    # left: 2 (reactor 2), middle: 3 (reactor1) ,right: 4 (reactor0)
 
-    switch(bus, _CHANNEL)
+    TCA9548A.switch(_CHANNEL)
