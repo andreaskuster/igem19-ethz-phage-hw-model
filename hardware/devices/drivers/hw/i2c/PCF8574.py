332dbfa9777738ffa84d094cdb03191b4ff4a0f0
andreas <mail@andreaskuster.ch>
2019-09-26 13:01:28 +0200

    Restructure PCF8574 for use in the driver level.

--- hardware/devices/drivers/hw/i2c/PCF8574.py
+++ hardware/devices/drivers/hw/i2c/PCF8574.py
@@ -1,86 +1,76 @@
 import time
+
 import smbus
 
-_DEVICE_ADDRESS = 0x38
 
+class PCF8574:
+    _BUS_NO = 1
+    _DEVICE_ADDRESS = 0x38
 
-def clear_all(bus: smbus):
-    """
-    Resets all eight I/Os to zero.
-    :param bus: smbus instance
-    """
-    bus.write_byte(_DEVICE_ADDRESS, 0x00)
+    bus = smbus.SMBus(_BUS_NO)
 
+    @staticmethod
+    def clear_all():
+        """
+        Resets all eight I/Os to zero.
+        """
+        PCF8574.bus.write_byte(PCF8574._DEVICE_ADDRESS, 0x00)
 
-def set_all(bus: smbus):
-    """
-    Sets all eight I/Os to one.
-    :param bus:
-    """
+    @staticmethod
+    def set_all():
+        """
+        Sets all eight I/Os to one.
+        """
+        PCF8574.bus.write_byte(PCF8574._DEVICE_ADDRESS, 0xff)
 
-    bus.write_byte(_DEVICE_ADDRESS, 0xff)
+    @staticmethod
+    def set(port: int):
+        """
+        Sets an individual I/O to one.
+        :param port: I/O port (0,..,7)
+        """
+        # read register
+        state = PCF8574.bus.read_byte(PCF8574._DEVICE_ADDRESS)
+        # set port
+        PCF8574.bus.write_byte(PCF8574._DEVICE_ADDRESS, state | (0x1 << port))
 
+    @staticmethod
+    def clear(port: int):
+        """
+        Clears an individual I/O to zero.
+        :param port: I/O port (0,..,7)
+        """
+        # read register
+        state = PCF8574.bus.read_byte(PCF8574._DEVICE_ADDRESS)
+        # clear port
+        PCF8574.bus.write_byte(PCF8574._DEVICE_ADDRESS, state & (0xff ^ (0x1 << port)))
 
-def set(bus: smbus,
-        port: int):
-    """
-    Sets an individual I/O to one.
-    :param bus: smbus instance
-    :param port: I/O port (0,..,7)
-    """
-    # read register
-    state = bus.read_byte(_DEVICE_ADDRESS)
-    # set port
-    bus.write_byte(_DEVICE_ADDRESS, state | (0x1 << port))
-
-
-def clear(bus: smbus,
-          port: int):
-    """
-    Clears an individual I/O to zero.
-    :param bus: smbus instance
-    :param port: I/O port (0,..,7)
-    """
-    # read register
-    state = bus.read_byte(_DEVICE_ADDRESS)
-    # clear port
-    bus.write_byte(_DEVICE_ADDRESS, state & (0xff ^ (0x1 << port)))
-
-
-def toggle(bus: smbus,
-          port: int):
-    """
-    Toggles an individual I/O port.
-    :param bus: smbus instance
-    :param port: I/O port (0,..,7)
-    """
-    # read register
-    state = bus.read_byte(_DEVICE_ADDRESS)
-    # clear port
-    bus.write_byte(_DEVICE_ADDRESS, state ^ (0x1 << port))
+    @staticmethod
+    def toggle(port: int):
+        """
+        Toggles an individual I/O port.
+        :param port: I/O port (0,..,7)
+        """
+        # read register
+        state = PCF8574.bus.read_byte(PCF8574._DEVICE_ADDRESS)
+        # clear port
+        PCF8574.bus.write_byte(PCF8574._DEVICE_ADDRESS, state ^ (0x1 << port))
 
 
 if __name__ == "__main__":
 
-    # instantiate i2c smbus
-    _BUS_NO = 1
-    bus = smbus.SMBus(_BUS_NO)
-
     _CASE = 2
 
     if _CASE == 1:
-
         # run all on / all off interval
         while True:
             print("set all outputs")
-            set_all(bus)
+            PCF8574.set_all()
             time.sleep(10)
             print("reset all outputs")
-            clear_all(bus)
+            PCF8574.clear_all()
             time.sleep(10)
-
     elif _CASE == 2:
-        clear_all(bus)
-    
+        PCF8574.clear_all()
 
 # note: PCF8574 uses 'sinking': 0 -> on, 1 -> off
