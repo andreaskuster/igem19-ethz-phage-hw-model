f793b8a25edfb1722ce0c96443f544d9c1ea385a
andreas <mail@andreaskuster.ch>
2019-09-26 09:36:18 +0200

    Modify implementation for adafruit_pca9685 library reuse.

--- hardware/hw/i2c/PCA9685.py
+++ hardware/hw/i2c/PCA9685.py
@@ -1,60 +1,70 @@
-from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
+from board import SCL, SDA
 
 
-def init(bus: busio) -> PCA9685:
-    pwm_driver = PCA9685(bus)
-    pwm_driver.frequency = 60
-    return pwm_driver
+class PCA9685:
+    lib = PCA9685(busio.I2C(SCL, SDA))
 
+    def __init__(self):
+        pass
 
-def set_pwm(lib: PCA9685,
-            channel: int,
-            value: int):
-    """
+    @staticmethod
+    def init():
+        PCA9685.lib.frequency = 60
 
-    :param lib: PCA9685 pwd driver library instance
-    :param channel: channel
-    :param value: pwm value (0x0000 - 0xffff)
-    """
-    # check input value
-    if value > 0xffff or value < 0x0000:
-        raise ValueError("Value out of bounds.")
-    if channel > 15 or channel < 0:
-        raise ValueError("Channel number out of bounds.")
+    @staticmethod
+    def set_pwm(channel: int,
+                value: int) -> None:
+        """
+        Set the PWM (pulse width modulation) value of a channel.
+        :param channel: channel
+        :param value: pwm value (0x0000 - 0xffff)
+        """
+        # check input value
+        if value > 0xffff or value < 0x0000:
+            raise ValueError("Value out of bounds.")
+        if channel > 15 or channel < 0:
+            raise ValueError("Channel number out of bounds.")
+        # check lib initialized
+        if PCA9685.lib is None:
+            raise RuntimeError("Module has not been initialized.")
+        # set pwm value
+        PCA9685.lib.channels[channel].duty_cycle = value
 
-    lib.channels[channel].duty_cycle = value
+    @staticmethod
+    def set_all(value: int) -> None:
+        """
+        Set the PWM value of all channels.
+        :param value: pwm value (0x0000 - 0xffff)
+        """
+        for i in range(16):
+            PCA9685.set_pwm(i, value)
 
 
 if __name__ == "__main__":
-
-    # Create the I2C bus interface.
-    i2c_bus = busio.I2C(SCL, SDA)
-
-    # Create a simple PCA9685 class instance.
-    pca = init(i2c_bus)
+    # initialize the PCA9685 board
+    PCA9685.init()
 
     # set all outputs to their default value
 
     # peristaltic pumps
-    value = 0xffff
-    set_pwm(pca, 0, 0x0000)
-    set_pwm(pca, 1, 0x0000)
-    set_pwm(pca, 2, 0x0000)
-    set_pwm(pca, 3, 0x0000)
-    set_pwm(pca, 4, 0x0000)
-    set_pwm(pca, 5, 0x0000)
-    set_pwm(pca, 12, 0x0000)
-    set_pwm(pca, 13, 0x0000)
-    set_pwm(pca, 14, 0x0000)
+    PCA9685.set_pwm(0, 0x0000)
+    PCA9685.set_pwm(1, 0x0000)
+    PCA9685.set_pwm(2, 0x0000)
+    PCA9685.set_pwm(3, 0x0000)
+    PCA9685.set_pwm(4, 0x0000)
+    PCA9685.set_pwm(5, 0x0000)
+    PCA9685.set_pwm(12, 0x0000)
+    PCA9685.set_pwm(13, 0x0000)
+    PCA9685.set_pwm(14, 0x0000)
 
     # pc fan
-    set_pwm(pca, 8, 0xffff)
-    set_pwm(pca, 9, 0xffff)
-    set_pwm(pca, 10, 0xffff)
+    PCA9685.set_pwm(8, 0xffff)
+    PCA9685.set_pwm(9, 0xffff)
+    PCA9685.set_pwm(10, 0xffff)
 
     # water pump
-    set_pwm(pca, 6, 0xffff)
-    set_pwm(pca, 7, 0x0000)
-    set_pwm(pca, 11, 0x0000)
+    PCA9685.set_pwm(6, 0xffff)
+    PCA9685.set_pwm(7, 0x0000)
+    PCA9685.set_pwm(11, 0x0000)
