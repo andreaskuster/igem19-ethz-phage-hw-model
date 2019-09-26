0080e12d3129a2df1356f9d26feac6b4c5d4eb49
Andreas Kuster <mail@andreaskuster.ch>
2019-09-26 12:32:12 +0200

    Change device from BMP280 to BME280.

--- hardware/devices/drivers/hw/i2c/BME280.py
+++ hardware/devices/drivers/hw/i2c/BME280.py
@@ -1,6 +1,6 @@
 import time
 
-import adafruit_bmp280
+import adafruit_bme280
 import board
 import busio
 
@@ -11,7 +11,7 @@ def get_temperature(bus):
     :param bus: busio instance
     :return: temperature in degree celcius
     """
-    bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
     return bmp280.temperature
 
 
@@ -21,17 +21,20 @@ def get_pressure(bus):
     :param bus: busio instance
     :return: pressure in hPa
     """
-    bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-    return bmp280.pressure
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+    return bme280.pressure
 
 
 if __name__ == "__main__":
 
     # Create library object using our Bus I2C port
     i2c = busio.I2C(board.SCL, board.SDA)
-    bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+
+    adafruit_bme280._BME280_ADDRESS = 0x76
+    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+    bme280._BME280_ADDRESS = 0x76
 
     while True:
-        print("\nTemperature: {} °C".format(bmp280.temperature))
-        print("Pressure: {} hPa".format(bmp280.pressure))
+        print("\nTemperature: {} °C".format(bme280.temperature))
+        print("Pressure: {} hPa".format(bme280.pressure))
         time.sleep(1.0)
