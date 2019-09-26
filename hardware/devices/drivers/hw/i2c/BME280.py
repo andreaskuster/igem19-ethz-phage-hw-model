321ad23006931ac7118c1c284dfe0bb2ce23c27a
andreas <mail@andreaskuster.ch>
2019-09-26 12:40:15 +0200

    Create class structure for library reuse.

--- hardware/devices/drivers/hw/i2c/BME280.py
+++ hardware/devices/drivers/hw/i2c/BME280.py
@@ -1,40 +1,36 @@
 import time
 
-import adafruit_bme280
-import board
 import busio
+from adafruit_bme280 import Adafruit_BME280_I2C
+from board import SCL, SDA
 
 
-def get_temperature(bus):
-    """
+class BME280:
+    lib = Adafruit_BME280_I2C(busio.I2C(SCL, SDA))
 
-    :param bus: busio instance
-    :return: temperature in degree celcius
-    """
-    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-    return bmp280.temperature
+    @staticmethod
+    def init():
+        pass
 
+    @staticmethod
+    def get_temperature() -> float:
+        """
+        
+        :return:
+        """
+        return BME280.lib.temperature
 
-def get_pressure(bus):
-    """
+    @staticmethod
+    def get_pressure() -> float:
+        """
 
-    :param bus: busio instance
-    :return: pressure in hPa
-    """
-    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-    return bme280.pressure
+        :return:
+        """
+        return BME280.lib.pressure
 
 
 if __name__ == "__main__":
-
-    # Create library object using our Bus I2C port
-    i2c = busio.I2C(board.SCL, board.SDA)
-
-    adafruit_bme280._BME280_ADDRESS = 0x76
-    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-    bme280._BME280_ADDRESS = 0x76
-
     while True:
-        print("\nTemperature: {} °C".format(bme280.temperature))
-        print("Pressure: {} hPa".format(bme280.pressure))
+        print("\nTemperature: {} °C".format(BME280.get_temperature()))
+        print("Pressure: {} hPa".format(BME280.get_pressure()))
         time.sleep(1.0)
