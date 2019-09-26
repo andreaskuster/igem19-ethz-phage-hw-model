e1e6c00c01e2dce5363ccb2b03c951eeec4f4e1f
andreas <mail@andreaskuster.ch>
2019-09-26 18:17:10 +0200

    Implement light sensor driver.

--- hardware/devices/drivers/LightSensor.py
+++ hardware/devices/drivers/LightSensor.py
@@ -1,11 +1,49 @@
-from enum import  Enum
+from __future__ import annotations
+
+import threading
+import time
+import warnings
+from enum import Enum
+
+from hw.i2c import TSL2591
 
 
 class LightSensor(Enum):
-
     REACTOR0 = 4
     REACTOR1 = 3
     REACTOR2 = 2
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 id: TSL2591,
+                 lock: threading.Lock):
+        """
+        Initialize the class structure.
+        :param id:
+        :param lock:
+        """
+        self.id = id
+        self.thread_safe = False if lock is None else True
+        if self.thread_safe:
+            self.lock = lock
+            with self.lock:
+                TSL2591.init()
+        else:
+            warnings.warn("Class functionality is not thread-safe.")
+            TSL2591.init()
+
+    def get_light_intensity(self):
+        if self.thread_safe:
+            with self.lock:
+                return TSL2591.read_light_intensity(self.id)
+        else:
+            return TSL2591.read_light_intensity(self.id)
+
+
+if __name__ == "__main__":
+    sensors = [LightSensor(LightSensor.REACTOR0),
+               LightSensor(LightSensor.REACTOR1),
+               LightSensor(LightSensor.REACTOR2)]
+    while True:
+        for sensor in sensors:
+            print("Light intensity of {}: {} units.".format(sensor.name, sensor.get_light_intensity()))
+        time.sleep(10.0)
