01251c22e1022465ecdca64f199c8d89c745f486
andreas <mail@andreaskuster.ch>
2019-09-26 11:40:31 +0200

    Create initial implementation of the peristaltic pump driver.

---
+++ hardware/devices/drivers/PeristalticPump.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class PeristalticPump(Enum):
+
+    PUMP0 = 0
+    PUMP1 = 1
+    PUMP2 = 2
+    PUMP3 = 3
+    PUMP4 = 4
+    PUMP5 = 5
+    PUMP6 = 12
+    PUMP7 = 13
+    PUMP8 = 14
+
+    def __init__(self):
+        pass
