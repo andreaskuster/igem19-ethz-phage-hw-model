ca6243c27788b80de730bd0f22ca4e9ff248c61a
andreas <mail@andreaskuster.ch>
2019-09-28 18:43:48 +0200

    Match all user input commands to the corresponding device function calls. Add device name to integer id mappings. Nest the whole command loop inside a try catch KeyboardInterrupt and handle proper program exit on keyboard interrupt (Ctrl + C).

--- reactor.py
+++ reactor.py
@@ -10,6 +10,31 @@ from hardware.devices.PeristalticPump import PeristalticPump
 from hardware.devices.ReactorTemperatureControl import ReactorTemperatureControl
 
 
+_PUMP_MAP = {
+    "pump0": 0,
+    "pump1": 1,
+    "pump2": 2,
+    "pump3": 3,
+    "pump4": 4,
+    "pump5": 5,
+    "pump6": 6,
+    "pump7": 7,
+    "pump8": 8,
+    "pump9": 9
+}
+
+_REACTOR_MAP = {
+    "reactor0": 0,
+    "reactor1": 1,
+    "reactor2": 2
+}
+
+_SENSOR_MAP = {
+    "od0": 0,
+    "od1": 1,
+    "od2": 2
+}
+
 def print_help():
     print("usage: reactor.py command arg0 arg1 ..")
     print("required: command")
@@ -21,7 +46,7 @@ def print_help():
     # peristaltic pumps
     print("set speed pump0 100.0 // [%]")
     print("disable speed pump0")
-    print("set volume pump0 50 // [ml]")
+    # print("set volume pump0 50 // [ml]") TODO
     # od sensor
     print("enable sensor od0")
     print("disable sensor od0")
@@ -149,38 +174,48 @@ if __name__ == "__main__":
     od_sensor1_event_loop_thread.start()
     od_sensor2_event_loop_thread.start()
 
-    while True:
-        if not sys.stdin.isatty():  # check if input is available
-            for line in sys.stdin:  # read lines
+    try:
+        while True:
+            if not sys.stdin.isatty():  # check if input is available
+                for line in sys.stdin:  # read lines
 
-                command = line.split()  # split command into components
+                    command = line.split()  # split command into components
 
-                # go through all command queries
-                try:
-                    if len(command) == 0 or command[0] == "help":
-                        print_help()
-                    elif command[0] == "enable":
-                        if command[1] == "sensor":
-                            pass  # TODO
-                    elif command[0] == "disable":
-                        if command[1] == "temperature":
-                            pass  # TODO
-                        elif command[1] == "speed":
-                            pass  # TODO
-                        elif command[1] == "sensor":
-                            pass  # TODO
-                    elif command[0] == "set":
-                        if command[1] == "temperature":
-                            pass  # TODO
-                        elif command[1] == "speed":
-                            pass  # TODO
-                        elif command[1] == "volume":
-                            pass  # TODO
-                    else:
-                        print_help()
-                except:
-                    print("Invalid syntax, try again.")
-        time.sleep(0.5)
+                    # go through all command queries
+                    try:
+                        if len(command) == 0 or command[0] == "help":
+                            print_help()
+                        elif command[0] == "enable":
+                            if command[1] == "sensor":
+                                od_sensor[_SENSOR_MAP[command[2]]].enable()
+                        elif command[0] == "disable":
+                            if command[1] == "temperature":
+                                reactor_temperature[_REACTOR_MAP[command[2]]].disable()
+                            elif command[1] == "speed":
+                                pump[_PUMP_MAP[command[2]]].disable()
+                            elif command[1] == "sensor":
+                                od_sensor[_SENSOR_MAP[command[2]]].disable()
+                        elif command[0] == "set":
+                            if command[1] == "temperature":
+                                reactor_temperature[_REACTOR_MAP[command[2]]].set_target_temperature(float(command[3]))
+                            elif command[1] == "speed":
+                                pump[_PUMP_MAP[command[2]]].set_speed(int(command[3]))
+                            elif command[1] == "volume":
+                                pump[_PUMP_MAP[command[2]]].set_volume(int(command[3]))
+                        else:
+                            print_help()
+                    except:
+                        print("Invalid syntax, try again.")
+            time.sleep(0.5)
+
+    except KeyboardInterrupt:
+        print("Exiting...but first put all devices into a safe state...")
+    finally:
+        # save log files: TODO
+        # put all devices into a safe idle state
+        print("Goodbye.")
+
+
 
 # https://docs.python.org/2/library/threading.html#using-locks-conditions-and-semaphores-in-the-with-statement
 # https://stackoverflow.com/questions/3762881/how-do-i-check-if-stdin-has-some-data
