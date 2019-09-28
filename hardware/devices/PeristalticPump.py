f8c2a60843d1916d688a89b94a432e13e0eeca8f
andreas <mail@andreaskuster.ch>
2019-09-28 18:45:27 +0200

    Add method structure for external callings.

---
+++ hardware/devices/PeristalticPump.py
@@ -0,0 +1,21 @@
+
+
+
+class PeristalticPump:
+
+    def __init__(self):
+        pass
+
+    def enable(self):
+        pass
+
+    def disable(self):
+        pass
+
+    def set_speed(self,
+                  value: int):
+        pass
+
+    def set_volume(self,
+                   value: int):
+        raise NotImplementedError()
