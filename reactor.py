1aebca4eb781fa6148d188e43380340802e8c60f
andreas <mail@andreaskuster.ch>
2019-09-28 11:27:09 +0200

    Implement interactive command line input.

--- reactor.py
+++ reactor.py
@@ -1,14 +1,25 @@
 # this is the top wrapper combining the hardware, model and real-time visualization.
 
 import sys
-import select
 import time
 
-def command1(test):
-    print("command1: {}".format(test))
 
-def command2():
-    print("command 2")
+def print_help():
+    print("usage: reactor.py command arg0 arg1 ..")
+    print("required: command")
+    print()
+    print("example commands:")
+    # temperature control
+    print("set temperature reactor0 39.0 // [°C]")
+    print("disable temperature reactor0")
+    # peristaltic pumps
+    print("set speed pump0 100.0 // [%]")
+    print("disable speed pump0")
+    print("set volume pump0 50 // [ml]")
+    # od sensor
+    print("enable sensor od0")
+    print("disable sensor od0")
+    print()
 
 
 def event_loop():
@@ -17,11 +28,31 @@ def event_loop():
 
             command = line.split()  # split command into components
 
-            if command[0] == "help":
-                pass
-            elif command[0] == "cmd2":
-                command2()
-
+            # go through all command queries
+            try:
+                if len(command) == 0 or command[0] == "help":
+                    print_help()
+                elif command[0] == "enable":
+                    if command[1] == "sensor":
+                        pass # TODO
+                elif command[0] == "disable":
+                    if command[1] == "temperature":
+                        pass # TODO
+                    elif command[1] == "speed":
+                        pass # TODO
+                    elif command[1] == "sensor":
+                        pass # TODO
+                elif command[0] == "set":
+                    if command[1] == "temperature":
+                        pass # TODO
+                    elif command[1] == "speed":
+                        pass # TODO
+                    elif command[1] == "volume":
+                        pass # TODO
+                else:
+                    print_help()
+            except:
+                print("Invalid syntax, try again.")
 
 if __name__ == "__main__":
     while True:
