f36ce10c58ea9300476e6e36abd2db500f0c01f8
andreas <mail@andreaskuster.ch>
2019-10-01 11:33:30 +0200

    Add default value for i2c lock. Fix device id mapping. Fix bug of parameter being float instead of int.

--- hardware/devices/drivers/WaterPump.py
+++ hardware/devices/drivers/WaterPump.py
@@ -15,13 +15,13 @@ from drivers.hw.i2c.PCA9685 import PCA9685
 class WaterPump:
     _DEVICE_ID_MAP = {
         0: 6,
-        1: 7,
-        2: 11
+        1: 11,
+        2: 7
     }
 
     def __init__(self,
                  id: int,
-                 i2c_lock: threading.Lock):
+                 i2c_lock: threading.Lock = None):
         self.id = id
         self.thread_safe = False if i2c_lock is None else True
         if self.thread_safe:
@@ -37,12 +37,12 @@ class WaterPump:
 
         :param value: speed value in percent
         """
-        scaled_value = (float(value) / 100.0) * 0xffff
+        scaled_value = int((float(value) / 100.0) * 0xffff)
         if self.thread_safe:
             with self.lock:
-                PCA9685.set_pwm(self.lib, self._DEVICE_ID_MAP[self.id], scaled_value)
+                PCA9685.set_pwm(self._DEVICE_ID_MAP[self.id], scaled_value)
         else:
-            PCA9685.set_pwm(self.lib, self._DEVICE_ID_MAP[self.id], scaled_value)
+            PCA9685.set_pwm(self._DEVICE_ID_MAP[self.id], scaled_value)
 
     def start(self):
         self.set_speed(100)
@@ -55,16 +55,17 @@ if __name__ == "__main__":
     pumps = [WaterPump(0),
              WaterPump(1),
              WaterPump(2)]
+    pumps = [WaterPump(2)]
     while True:
         print("Set speed of all pumps to 100%")
         for pump in pumps:
             pump.set_speed(100)
-        time.sleep(10)
+        time.sleep(1)
         print("Set speed of all pumps to 50%")
         for pump in pumps:
             pump.set_speed(50)
-        time.sleep(10)
+        time.sleep(1)
         print("Set speed of all pumps to 0%")
         for pump in pumps:
             pump.set_speed(0)
-        time.sleep(10)
+        time.sleep(1)
