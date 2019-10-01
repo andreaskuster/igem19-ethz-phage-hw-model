e8adc6d040b826b28bf31d5aa742ef2bf8aaa6fd
mike <michael.spahr@alumni.epfl.ch>
2019-10-01 13:10:14 +0200

    Add all differential equations for reactor B

--- recombineering/reactorB_dde.py
+++ recombineering/reactorB_dde.py
@@ -11,7 +11,8 @@ from phage import Phage
 # simulation time and resolution of samples
 xs = np.linspace(0, 120, 100)
 
-flux= 0.0
+fluxA= 0.0
+fluxB= 0.0
 #Reactor A
 # lb influx profile of reactor A
 def in_a_lb(t):
@@ -19,7 +20,7 @@ def in_a_lb(t):
     :param t: time t
     :return: lb influx to reactor a at time t
     """
-    return flux
+    return fluxA
     
 # biomass outflux profile of reactor A
 def out_a(t):
@@ -27,7 +28,7 @@ def out_a(t):
     :param t: time t
     :return: biomass outflux of reactor a at time t
     """
-    return flux
+    return fluxA
     
 # temperature profile of reactor A
 def temperature_a(t):
@@ -44,7 +45,7 @@ def in_b_lb(t):
     :param t: time t
     :return: lb influx to reactor b at time t
     """
-    return 0.007
+    return fluxB/3
     
 # lb influx profile of reactor B
 def in_nh(t):
@@ -52,7 +53,7 @@ def in_nh(t):
     :param t: time t
     :return: new_host influx from reactor a to reactor b at time t
     """
-    return 0.007
+    return fluxB/3
 
 # phage library influx in reactor B
 def in_lib(t):
@@ -61,7 +62,7 @@ def in_lib(t):
     :return: library influx in reator b at time t
     """
     
-    return 0.007
+    return fluxB/3
     
 # biomass outflux profile of reactor B
 def out_b(t):
@@ -69,7 +70,7 @@ def out_b(t):
     :param t: time t
     :return: biomass outflux of reactor b at time t
     """
-    return 0.007
+    return fluxB
 
 
 # temperature profile of reactor B
@@ -106,13 +107,13 @@ new_host = Host(
 
 original_phage = Phage(
     c0 = 1000,
-    adsorption_rate = 0.0000000001,
+    adsorption_rate = 0.00000000001,
     burst_size = 100,
     death_rate = 0.00272,
 )
 new_phage = Phage(
     c0 = 1,
-    adsorption_rate = 0.00000001,
+    adsorption_rate = 0.0000001,
     burst_size = 100,
     death_rate = 0.00272,
 )
@@ -121,7 +122,7 @@ s0 = 0.0000025 #stock concentration of nutrient (g/mL)#0.0000025
 R_pnn = 1/1000 # fraction of new phage in library
 
 # define system of differential equations
-"""
+
 def dXa_dt(X, t):
     [c_host_a, c_nutr_a] = X
     return np.array([
@@ -144,24 +145,32 @@ def dXb_dt(X, t):#TODO add delay diff term for infected host and phages, change 
         0 if c_pnn <= 0 else new_phage.burst_size*c_host_inf_pnn - new_phage.infection_rate(c_host_b)
         - out_b(t)*c_pnn - new_phage.death_rate*c_pnn + in_lib(t)*R_pnn #new phage concentration
     ])
-"""
 
-n=2
-reactorA = [new_host.per_cell_growth_rate(y(1,t-1),37)*y(0) - out_a(t)*y(0) -new_host.death_rate*y(0),
-            - new_host.yield_coeff*new_host.per_cell_growth_rate(y(1,t-1),37)*y(0) + s0*in_a_lb(t) - y(1)*out_a(t)]
+
+n=8
+l=20
+reactorA = [new_host.per_cell_growth_rate(y(1),temperature_b(t))*y(0) - out_a(t)*y(0) -new_host.death_rate*y(0),#c_host_a
+            - new_host.yield_coeff*new_host.per_cell_growth_rate(y(1),temperature_b(t))*y(0) + s0*in_a_lb(t) - y(1)*out_a(t),#c_nutr_a
+            new_host.per_cell_growth_rate(y(3),temperature_b(t))*y(2) + y(0)*in_nh(t) - new_phage.infection_rate(y(2)) - original_phage.infection_rate(y(2)) - new_host.death_rate*y(2) - out_b(t)*y(2),#c_host_b
+            - new_host.yield_coeff*new_host.per_cell_growth_rate(y(3),temperature_b(t))*(y(2)+y(4)+y(5)) + y(1)*in_nh(t) + s0*in_b_lb(t) - y(3)*out_b(t),#c_nutr_b
+            original_phage.infection_rate(y(2)) - out_b(t)*y(4),#host infected by original phage
+            new_phage.infection_rate(y(2)) - out_b(t)*y(5),#host infected by new phage
+            original_phage.burst_size*y(4,t-l) - original_phage.infection_rate(y(2)) - out_b(t)*y(6) - original_phage.death_rate*y(6) + in_lib(t)*(1 - R_pnn),#c_poo
+            new_phage.burst_size*y(5,t-l) - new_phage.infection_rate(y(2)) - out_b(t)*y(7) - new_phage.death_rate*y(7) + in_lib(t)*(1 - R_pnn)]#c_pnn
 def dX():
     for i in range(n):
         yield reactorA[i]
 
-initial_state = np.array([new_host.c0,s0])
+#initial_state = np.array([new_host.c0,s0,new_host.c0,s0,0.0,0.0,original_phage.c0,new_phage.c0])
+initial_state = np.array([new_host.c0,s0,new_host.c0,s0,0.0,0.0,original_phage.c0,new_phage.c0])
 DDE = jitcdde(dX, verbose=True)
 
-DDE.constant_past([new_host.c0,s0])
-
+#DDE.constant_past([new_host.c0,s0,new_host.c0,s0,0.0,0.0,original_phage.c0,new_phage.c0])
+DDE.constant_past([new_host.c0,s0,new_host.c0,s0,0.0,0.0,original_phage.c0,new_phage.c0])
 DDE.step_on_discontinuities()
 
 data = []
-x = np.arange(DDE.t, DDE.t+120, 120/100)
+x = np.arange(DDE.t, DDE.t+220, 120/100)
 for time in x:
 	data.append( DDE.integrate(time) )
 np.savetxt("timeseries.dat", data)
@@ -188,13 +197,57 @@ c_nutrient_a = [y[1] for y in ys]
 plt.figure(figsize=(16, 16))
 
 
-plt.subplot(3, 3, 4)
+plt.subplot(3, 3, 1)
 plt.plot(x, [data[t][0] for t in range(len(x))], label="reactor A")
 plt.xlabel('time [min]')
+plt.ylabel('conc')
+plt.title('Host Concentration A over Time')
+plt.legend()
+plt.subplot(3, 3, 2)
+plt.plot(x, [data[t][1] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
+plt.ylabel('conc')
+plt.title('Nutrient A over Time')
+plt.legend()
+plt.subplot(3, 3, 3)
+plt.plot(x, [data[t][2] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
+plt.ylabel('conc')
+plt.title('Host Concentration B over Time')
+plt.legend()
+plt.subplot(3, 3, 4)
+plt.plot(x, [data[t][3] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
 plt.ylabel('temperature [°C]')
-plt.title('Host Concentration over Time')
+plt.title('Nutrient B over Time')
 plt.legend()
 
+plt.subplot(3, 3, 5)
+plt.plot(x, [data[t][4] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
+plt.ylabel('temperature [°C]')
+plt.title('Host infected OO over Time')
+plt.legend()
+plt.subplot(3, 3, 6)
+plt.plot(x, [data[t][5] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
+plt.ylabel('temperature [°C]')
+plt.title('Host infected NN over Time')
+plt.legend()
+plt.subplot(3, 3, 7)
+plt.plot(x, [data[t][6] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
+plt.ylabel('temperature [°C]')
+plt.title('Phage OO over Time')
+plt.legend()
+plt.subplot(3, 3, 8)
+plt.plot(x, [data[t][7] for t in range(len(x))], label="reactor A")
+plt.xlabel('time [min]')
+plt.ylabel('temperature [°C]')
+plt.title('Phage NN over Time')
+plt.legend()
+
+"""
 plt.subplot(3, 3, 5)
 plt.plot(x, [new_host.per_cell_growth_rate(data[t][1],temperature_a(t)) for t in range(len(x))], label="new host")
 plt.xlabel('time [min]')
@@ -231,7 +284,7 @@ plt.xlabel('time [min]')
 plt.ylabel('biomass outflux [ml]')
 plt.title('Biomass Outflux over Time')
 plt.legend()
-
+"""
 """
 plt.subplot(3, 3, 4)
 plt.plot(xs, [y[0] for y in ys], label="new host")
@@ -297,7 +350,7 @@ plt.title('Phage new')
 plt.legend()
 
 """
-
+"""
 plt.subplot(3, 3, 7)
 nutrient_x = np.linspace(0, 0.0001)
 plt.plot(nutrient_x, [new_host.per_cell_growth_rate(x,37) for x in nutrient_x], label="new host")
@@ -328,6 +381,6 @@ plt.xlabel('nutrient concentration [g/mL]')
 plt.ylabel('per cell growth rate')
 plt.title('Growth Rate at high conc. of nutrient')
 plt.legend()
-
+"""
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 plt.show()
