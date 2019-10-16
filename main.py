311b670ed4d6333a7348de984619fd125ea000e5
andreas <mail@andreaskuster.ch>
2019-10-16 11:11:28 +0200

    Fix bug with negative value.

--- main.py
+++ main.py
@@ -124,14 +124,14 @@ class AdvancedNaiveConstantConcentration:
             if diff > self.tol:
                 self.pumps[1].enable()
                 self.pumps[3].enable()
-                time.sleep(diff * 100)
+                time.sleep(max(8.0, diff * 100))
                 self.pumps[1].disable()
                 self.pumps[3].disable()
             elif diff < -self.tol:
                 self.pumps[0].enable()
                 self.pumps[2].enable()
                 self.pumps[3].enable()
-                time.sleep(diff * 100)
+                time.sleep(max(8.0, abs(diff * 100)))
                 self.pumps[0].disable()
                 self.pumps[2].disable()
                 self.pumps[3].disable()
