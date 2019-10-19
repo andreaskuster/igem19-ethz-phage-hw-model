22819d7a29a061cd1098f4925faebad8aeb2f5da
andreas <mail@andreaskuster.ch>
2019-10-19 23:49:28 +0200

    Clean up code for pid controller plots.

--- hardware/pid_plot.py
+++ hardware/pid_plot.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(20,10))
 
-data_temperature = np.genfromtxt('temperature22.csv', delimiter=',')
-data_control_value = np.genfromtxt('control_value22.csv', delimiter=',')
-data_kp = np.genfromtxt('kp22.csv', delimiter=',')
-data_ki = np.genfromtxt('ki22.csv', delimiter=',')
-data_kd = np.genfromtxt('kd22.csv', delimiter=',')
+data_temperature = np.genfromtxt('data/temperature22.csv', delimiter=',')
+data_control_value = np.genfromtxt('data/control_value22.csv', delimiter=',')
+data_kp = np.genfromtxt('data/kp22.csv', delimiter=',')
+data_ki = np.genfromtxt('data/ki22.csv', delimiter=',')
+data_kd = np.genfromtxt('data/kd22.csv', delimiter=',')
 
 
 x = np.arange(len(data_temperature))
@@ -15,13 +15,15 @@ x = np.arange(len(data_temperature))
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-
-ax.plot(x, data_temperature, c='b', label='Temperature')
+X = [val/120.0 for val in x]
+ax.plot(X, data_temperature, c='b', label='Temperature')
 #ax.plot(x, data_control_value, c='g', label='Control Value')
 #ax.plot(x, data_kp, c='k', label='Kp')
 #ax.plot(x, data_ki, c='r', label='Ki')
 #ax.plot(x, data_kd, c='m', label='Kd')
-ax.plot(x,[22.0]*len(data_temperature), c='k', label='Target Temperature')
+ax.plot(X,[22.2]*len(data_temperature), c='k', label='Target Temperature')
+ax.set_xlabel('Time [min]')
+ax.set_ylabel('Temperature [°C]')
 
 plt.legend()
 plt.show()
