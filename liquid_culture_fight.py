b60875bb96df7ac44cf7b1a3677b5aa5e0e70b98
andreas <mail@andreaskuster.ch>
2019-10-19 14:47:20 +0200

    Add case study simulation.

--- liquid_culture_fight.py
+++ liquid_culture_fight.py
@@ -1,5 +1,5 @@
 from ddeint import ddeint
-from pylab import array, linspace, subplots
+from pylab import array, linspace, subplots, subplot
 
 
 def values_before_zero(t):
@@ -15,14 +15,26 @@ def model(Y, t):
     ])
 
 
-tt = linspace(0, 1e2, 1000)
-#tt = linspace(0, 6*1e2, 1000)
+tt0 = linspace(0, 6*1e1, 1000)
+tt1 = linspace(0, 6*1e2, 1000)
 
-yy = ddeint(model, values_before_zero, tt)
+yy0 = ddeint(model, values_before_zero, tt0)
+yy1 = ddeint(model, values_before_zero, tt1)
 
-fig, ax = subplots(1, figsize=(8, 4))
 
-ax.plot(tt, [x[0] for x in yy], label="the weak")
-ax.plot(tt, [x[1] for x in yy], label="the strong")
-fig.legend(loc='upper center', borderaxespad=2.0)
+fig, (ax0, ax1) = subplots(2, figsize=(8, 6))
+
+ax0.plot(tt0, [x[0] for x in yy0], label="slower growing, quantitatively more")
+ax0.ticklabel_format(axis='y', style='sci', scilimits=(2,3))
+ax0.plot(tt0, [x[1] for x in yy0], label="faster growing, quantitatively less")
+ax0.set_xlabel('Time [min]')
+ax0.set_ylabel('Number of Phages')
+ax0.legend(loc='upper left', borderaxespad=2.0)
+
+
+ax1.plot(tt1, [x[0] for x in yy1], label="slower growing, quantitatively more")
+ax1.plot(tt1, [x[1] for x in yy1], label="faster growing, quantitatively less")
+ax1.set_xlabel('Time [min]')
+ax1.set_ylabel('Number of Phages')
+ax1.legend(loc='upper left', borderaxespad=2.0)
 fig.show()
